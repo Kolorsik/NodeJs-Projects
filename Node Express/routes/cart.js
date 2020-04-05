f8bf4d331c19d0a020d376d3ec935f0ac7da0d3d
Kolorsik <roman.kukla@mail.ru>
2020-04-05 16:01:54 +0300

    new features

---
+++ Node Express/routes/cart.js
@@ -0,0 +1,10 @@
+const {Router} = require('express')
+const Cart = require('../models/cart')
+const Course = require('../models/course')
+const router = Router()
+
+router.post('/add', async (req, res) => {
+    //q
+})
+
+module.exports = router
